82a52b758632a4481c8f285a877839b4f2168b02
Lazaros Tsochatzidis <lazaros.tso@gmail.com>
2023-02-28 10:08:18 +0200

    clean up

--- datasets/classification_dataset.py
+++ datasets/classification_dataset.py
@@ -1,5 +1,6 @@
 from datasets.generic_dataset import CBISDDSMGenericDataset
 
+
 class CBISDDSMClassificationDataset(CBISDDSMGenericDataset):
     def __init__(self, dataframe, download_path, label_field, label_list, transform=None):
         super().__init__(dataframe, download_path, transform=transform)
@@ -10,18 +11,8 @@ class CBISDDSMClassificationDataset(CBISDDSMGenericDataset):
     def __getitem__(self, idx):
         image_tensor, item = super().__getitem__(idx)
 
-        # abnorm_w = (item['maxx'] - item['minx']) / 2
-        # abnorm_x = int(abnorm_w + item['minx'])
-        # abnorm_h = (item['maxy'] - item['miny']) / 2
-        # abnorm_y = int(abnorm_h + item['miny'])
-
         label_full = item[self.label_field]
         label = self.label_list.index(label_full)
-
-        # if self.transform:
-        #     image = self.transform(image)
-        # if self.target_transform:
-        #     label = self.target_transform(label)
 
         return image_tensor, label
 
--- datasets/generic_dataset.py
+++ datasets/generic_dataset.py
@@ -1,6 +1,4 @@
 import os
-from typing import Any, Tuple
-
 from torch.utils.data import Dataset
 from PIL import Image
 import numpy as np
