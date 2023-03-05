3e7e87725c50a4e6527fbe85aef6cd4ba5a34f22
Lazaros Tsochatzidis <lazaros.tso@gmail.com>
2023-03-05 13:20:39 +0200

    added whole image dataset example

---
+++ examples/whole_image_classification_dataset.py
@@ -0,0 +1,10 @@
+from ddsm_dataset_factory import CBISDDSMDatasetFactory
+
+dataset = CBISDDSMDatasetFactory('./config.json') \
+        .train() \
+        .add_masses() \
+        .drop_attributes("assessment", "breast_density", "subtlety") \
+        .map_attribute_value('pathology', {'BENIGN_WITHOUT_CALLBACK': 'BENIGN'}) \
+        .show_counts() \
+        .create_classification('pathology')
+dataset.visualize()
