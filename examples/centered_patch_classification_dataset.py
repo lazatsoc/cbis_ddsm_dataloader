f34eb669a38027f52ad97020a802227976e8ffb9
Lazaros Tsochatzidis <lazaros.tso@gmail.com>
2023-10-27 19:12:11 +0300

    fix centered patch size inconsistencies

--- examples/centered_patch_classification_dataset.py
+++ examples/centered_patch_classification_dataset.py
@@ -6,6 +6,6 @@ dataset = CBISDDSMDatasetFactory('./config.json') \
         .drop_attributes("assessment", "breast_density", "subtlety") \
         .map_attribute_value('pathology', {'BENIGN_WITHOUT_CALLBACK': 'BENIGN'}) \
         .show_counts() \
-        .lesion_patches_centered() \
+        .lesion_patches_centered((512,512)) \
         .create_classification('pathology')
 dataset.visualize()
